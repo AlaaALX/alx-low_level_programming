da170b6338373ee9e1f17d3de337de87d91d56d6
AlaaALX <dhamad576@gmail.com>
2023-02-23 08:26:07 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,16 +11,10 @@ void print_most_numbers(void)
 
 	int x = 0;
 
-	for (; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
+		if (!(x == '2' || x == '4'))
+			_putchar(x);
 		}
-		else
-		{
-			_putchar(x = '0');
-		}
-	}
 	_putchar('\n');
 }
