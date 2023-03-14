5cc21fea12b329b5dddcb8e924b9ba3b3e7f8912
AlaaALX <dhamad576@gmail.com>
2023-03-14 11:46:35 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A function that returns a pointer to
+ * a newly allocated space in memory
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	char *s;
+
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n";
+				return (1);
+				}
+				printf("%s\n", s);
+				free(s);
+				return (0);
+				}
